80ec651382bf4386d8fb64ae30616eeac7785678
KJachimstal <k,jachimstal@gmail.com>
2019-03-18 14:49:08 +0100

    Add comment and Label

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -3,13 +3,31 @@ import javax.swing.*;
 public class MainFrame extends JFrame {
 
     public MainFrame(){
+
+        //Frame settings
         setSize(800, 600);
         setTitle("AES Encryption and Descryption");
         setResizable(false);
         setLayout(null);
+
+        //Buttons settings
         JButton encryptionButton = new JButton("Encrypt");
         encryptionButton.setBounds(70, 460, 100,40);
         add(encryptionButton);
+
+        JButton decriptionButton = new JButton("Decrypt");
+        decriptionButton.setBounds(170, 460, 100,40);
+        add(decriptionButton);
+
+        //Text field settings
+
+
+        //Label settings
+        JLabel lCrypt;
+        lCrypt = new JLabel("Tekst do szyfrowania:");
+        lCrypt.setBounds(30, 0, 140, 30);
+        add(lCrypt);
+
     }
 
 
