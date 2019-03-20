b669da664d1d7957934bc0d10034ac679bf69e09
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-20 19:47:37 +0100

    Change date to two-dimensional array

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -1,23 +1,29 @@
 public class Block {
 
-    private byte[] data;
+    private byte[][] data;
 
-    public Block(byte[] data) {
+    public Block(byte[][] data) {
         this.data = fillBytes(data);
     }
 
-    private byte[] fillBytes(byte[] data) {
+    private byte[][] fillBytes(byte[][] data) {
         if (data.length == 16) {
             return data;
         }
-        byte[] output = new byte[16];
-        for(int i = 0; i < 16; i++) {
-            output[i] = (i > data.length - 1 ? 0 : data[i]);
+        byte[][] output = new byte[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i >= data.length || j >= data[i].length) {
+                    output[i][j] = 0;
+                } else {
+                    output[i][j] = data[i][j];
+                }
+            }
         }
         return output;
     }
 
-    public byte[] getData() {
+    public byte[][] getData() {
         return data;
     }
 }
--- src/test/java/BlockTest.java
+++ src/test/java/BlockTest.java
@@ -9,26 +9,29 @@ class BlockTest {
 
     @BeforeEach
     void initialize() {
-        block = new Block(new byte[] {
-                11, -50, 37, 81,
-                20, -46, -81, 70,
-                55, 21, -66, 23,
-                77, 74, -21, 1
+        block = new Block(new byte[][] {
+                { 11, -50, 37, 81 },
+                { 20, -46, -81, 70 },
+                { 55, 21, -66, 23 },
+                { 77, 74, -21, 1 }
         });
     }
 
     @Test
     void getData() {
-        assertEquals(16, block.getData().length);
+        assertEquals(4, block.getData().length);
+        assertEquals(4, block.getData()[0].length);
     }
 
     @Test
     void fillBytes() {
-        Block invalidBlock = new Block(new byte[] {
-                11, -50, 37, 81,
-                20, -46, -81, 70,
-                55, 21, -66, 23
+        Block invalidBlock = new Block(new byte[][] {
+                { 11, -50, 37, 81 },
+                { 20, -46, -81, 70 },
+                { 55, 21, -66, 23 },
+                { 77, 74 }
         });
-        assertEquals(16, invalidBlock.getData().length);
+        assertEquals(4, invalidBlock.getData().length);
+        assertEquals(4, invalidBlock.getData()[0].length);
     }
 }
