5ca51491d141e48c2a170509ff685c08cc6a6328
KJachimstal <k,jachimstal@gmail.com>
2019-03-20 15:51:48 +0100

    Initialize block test

---
+++ src/test/java/BlockTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockTest {
+
+    private Block block;
+
+    @BeforeEach
+    void initialize() {
+        block = new Block(new byte[] {
+                11, -50, 37, 81,
+                20, -46, -81, 70,
+                55, 21, -66, 23,
+                77, 74, -21, 1
+        });
+    }
+
+    @Test
+    void fillBytes() {
+
+    }
+
+}
