edc6eb01d80f3261ed94b8ccbfa367d7729ec635
KJachimstal <k,jachimstal@gmail.com>
2019-03-20 15:35:18 +0100

    Add fillBytes method to block class

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -2,7 +2,14 @@ public class Block {
 
     private byte[] data;
 
-    public Block(byte[] data){
-        this.data = data;
+    public Block(byte[] data) {
+        this.data = fillBytes(data);
+    }
+
+    private byte[] fillBytes(byte[] data) {
+        for(int i = data.length - 1; i < 16; i++) {
+            data[i] = 0;
+        }
+        return data;
     }
 }
