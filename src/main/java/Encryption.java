d66c55635a29ba04be5569a9681b77cbf5515637
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-23 20:57:47 +0100

    Add cryptography parent class

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -1,18 +1,7 @@
-public class Encryption {
-
-    private Block[] blocks;
-    private int rounds;
-    private RoundKeys roundKeys;
+public class Encryption extends Cryptography {
 
     public Encryption(Block[] blocks, Key key) {
-//        Set blocks
-        this.blocks = blocks;
-
-//        Get rounds count from key
-        rounds = key.getRounds();
-
-//        Create round keys
-        roundKeys = new RoundKeys(key);
+        super(blocks, key);
     }
 
     public void encrypt() {
@@ -44,39 +33,4 @@ public class Encryption {
         addRoundKey(data, round);
     }
 
-    public void subBytes(short[][] data) {
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                data[i][j] = Operations.subByte(data[i][j]);
-            }
-        }
-    }
-
-    public void shiftRows(short[][] data) {
-        for (int i = 0; i < data.length; i++) {
-            Operations.shiftRow(data[i], i);
-        }
-    }
-
-    public void mixColumns(short[][] data) {
-        for (int i = 0; i < data.length; i++) {
-            short[] tmp = new short[data.length];
-            for (int j = 0; j < data[0].length; j++) {
-                tmp[j] = data[j][i];
-            }
-
-            short[] mixed = Operations.mixColumn(tmp);
-            for (int j = 0; j < data[0].length; j++) {
-                data[j][i] = mixed[j];
-            }
-        }
-    }
-
-    public void addRoundKey(short[][] data, int round) {
-        Operations.addRoundKey(data, roundKeys, round);
-    }
-
-    public Block[] getBlocks() {
-        return blocks;
-    }
 }
--- src/test/java/EncryptionTest.java
+++ src/test/java/EncryptionTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
