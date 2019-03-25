d5408a375032e6cb77641ed8525755cc40dc955f
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-25 15:04:04 +0100

    Add menu to application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,26 +1,29 @@
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
-import java.awt.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Application {
-
+//    Frame
+    JFrame frame;
+//    Main panel
     public JPanel mainPanel;
+
     private JButton selectFile;
     private JButton button1;
     private JTextArea log;
     private DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    public Application() {
+    public Application(JFrame frame) {
+        this.frame = frame;
+        createMenu();
+
         DefaultCaret caret = (DefaultCaret) log.getCaret();
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 
         selectFile.addActionListener(e -> openDialog());
         button1.addActionListener(e -> test());
-        log("Application started");
-
     }
 
     public void openDialog() {
@@ -38,5 +41,17 @@ public class Application {
     private void log(String message) {
         Date date = new Date();
         log.append('[' + dateFormat.format(date) + "]: " + message + '\n');
+    }
+
+    private void createMenu() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu menu = new JMenu("File");
+        menuBar.add(menu);
+
+        JMenuItem menuItem = new JMenuItem("...");
+        menu.add(menuItem);
+
+        frame.setJMenuBar(menuBar);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,8 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("AES Encryption & Decryption");
-        frame.setContentPane(new Application().mainPanel);
+        Application application = new Application(frame);
+        frame.setContentPane(application.mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationByPlatform(true);
