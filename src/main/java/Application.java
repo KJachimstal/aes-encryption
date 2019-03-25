2e48560cb4238487bdd579f04469da9fbddd033f
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-25 14:57:13 +0100

    Add log to application form

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,26 @@
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+import java.awt.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Application {
 
     public JPanel mainPanel;
     private JButton selectFile;
+    private JButton button1;
+    private JTextArea log;
+    private DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     public Application() {
+        DefaultCaret caret = (DefaultCaret) log.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+
         selectFile.addActionListener(e -> openDialog());
+        button1.addActionListener(e -> test());
+        log("Application started");
+
     }
 
     public void openDialog() {
@@ -15,5 +29,14 @@ public class Application {
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             System.out.println(fc.getSelectedFile().getName());
         }
+    }
+
+    public void test() {
+        log("a");
+    }
+
+    private void log(String message) {
+        Date date = new Date();
+        log.append('[' + dateFormat.format(date) + "]: " + message + '\n');
     }
 }
